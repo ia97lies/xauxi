9fcf8f6f0143d56545446dd155edd80b71e79cfc
Christian Liesch <ia97lies@users.sourceforge.net>
2012-07-13 23:42:33 +0200

    start with experimental lua reverse proxy, just for fun

---
+++ src/xauxi.c
@@ -0,0 +1,172 @@
+/**
+ * Copyright 2012 Christian Liesch
+ *
+ * fooLicensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file
+ *
+ * @Author christian liesch <liesch@gmx.ch>
+ *
+ * Implementation of the lua based reverse proxy xauxi.
+ */
+
+/* affects include files on Solaris */
+#define BSD_COMP
+
+/************************************************************************
+ * Includes
+ ***********************************************************************/
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+/* Use STACK from openssl to sort commands */
+#include <openssl/ssl.h>
+
+#include <apr.h>
+#include <apr_signal.h>
+#include <apr_strings.h>
+#include <apr_network_io.h>
+#include <apr_file_io.h>
+#include <apr_time.h>
+#include <apr_getopt.h>
+#include <apr_general.h>
+#include <apr_lib.h>
+#include <apr_portable.h>
+#include <apr_support.h>
+#include <apr_hash.h>
+#include <apr_env.h>
+
+#include <pcre.h>
+
+#if APR_HAVE_UNISTD_H
+#include <unistd.h> /* for getpid() */
+#endif
+
+/************************************************************************
+ * Defines 
+ ***********************************************************************/
+
+/************************************************************************
+ * Structurs
+ ***********************************************************************/
+
+/************************************************************************
+ * Globals 
+ ***********************************************************************/
+
+apr_getopt_option_t options[] = {
+  { "version", 'V', 0, "Print version number and exit" },
+  { "help", 'h', 0, "Display usage information (this message)" },
+  { NULL, 0, 0, NULL }
+};
+
+/************************************************************************
+ * Privates
+ ***********************************************************************/
+
+/** 
+ * display usage information
+ *
+ * @progname IN name of the programm
+ */
+static void usage() {
+  int i = 0;
+
+  fprintf(stdout, "xauxi is a lua base reverse proxy\n");
+  fprintf(stdout, "\nUsage: xauxi [OPTIONS] scripts\n");
+  fprintf(stdout, "\nOptions:");
+  while (options[i].optch) {
+    if (options[i].optch <= 255) {
+      fprintf(stdout, "\n  -%c --%-15s %s", options[i].optch, options[i].name,
+	      options[i].description);
+    }
+    else {
+      fprintf(stdout, "\n     --%-15s %s", options[i].name, 
+	      options[i].description);
+    }
+    i++;
+  }
+
+  fprintf(stdout, "\n");
+  fprintf(stdout, "\nReport bugs to http://sourceforge.net/projects/xauxi");
+  fprintf(stdout, "\n");
+}
+
+/**
+ * display copyright information
+ *
+ * @param program name
+ */
+void copyright() {
+  printf("xauxi " PACKAGE_VERSION "\n");
+  printf("\nCopyright (C) 2012 Free Software Foundation, Inc.\n"
+         "This is free software; see the source for copying conditions.  There is NO\n"
+	 "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n");
+  printf("\nWritten by Christian Liesch\n");
+}
+
+/** 
+ * sort out command-line args and call test 
+ *
+ * @param argc IN number of arguments
+ * @param argv IN argument array
+ *
+ * @return 0 if success
+ */
+int main(int argc, const char *const argv[]) {
+  apr_status_t status;
+  apr_getopt_t *opt;
+  const char *optarg;
+  int c;
+  apr_pool_t *pool;
+
+  srand(apr_time_now()); 
+  
+  apr_app_initialize(&argc, &argv, NULL);
+  apr_pool_create(&pool, NULL);
+
+  /* block broken pipe signal */
+#if !defined(WIN32)
+  apr_signal_block(SIGPIPE);
+#endif
+  
+  /* set default */
+
+  /* create a global vars table */
+
+  /* get options */
+  apr_getopt_init(&opt, pool, argc, argv);
+  while ((status = apr_getopt_long(opt, options, &c, &optarg)) == APR_SUCCESS) {
+    switch (c) {
+    case 'h':
+      usage();
+      exit(0);
+      break;
+    case 'V':
+      copyright();
+      exit(0);
+      break;
+    }
+  }
+
+  /* test for wrong options */
+  if (!APR_STATUS_IS_EOF(status)) {
+    fprintf(stderr, "try \"xauxi --help\" to get more information\n");
+    exit(1);
+  }
+
+  return 0;
+}
