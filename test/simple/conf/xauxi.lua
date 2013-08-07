74ef0390583dc801c9e6ab8d6a8ba7290b5e37a6
Christian Liesch <ia97lies@users.sourceforge.net>
2013-08-07 13:41:18 +0200

    realy simple one

--- test/simple/conf/xauxi.lua
+++ test/simple/conf/xauxi.lua
@@ -1,8 +1,7 @@
 -- Frist simple proxy configuration 
-listen("http://localhost:8080", function(Connection connection)
-  connection.dispatcher("path=/foo",
-  function(Request request)
-    request.connect("http://localhost:9080")
+function global()
+  listen("http://localhost:8080", 
+  function()
+    print("Got a connection on 8080");
   end)
-end)
-
+end
