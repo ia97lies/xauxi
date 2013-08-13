27d07df8adc191f342275f64a73c392c82288319
Christian Liesch <ia97lies@users.sourceforge.net>
2013-08-13 23:00:30 +0200

    remove schema, will introduce this later

--- test/simple/conf/xauxi.lua
+++ test/simple/conf/xauxi.lua
@@ -1,6 +1,6 @@
 -- Frist simple proxy configuration 
 function global()
-  listen("http://localhost:8080", 
+  listen("localhost:8080", 
   function()
     print("Got a connection on 8080");
   end)
