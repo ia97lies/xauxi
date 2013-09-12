926b5933a4f612b5e66e397dffcf1b6830c0057f
Christian Liesch <ia97lies@users.sourceforge.net>
2013-09-12 17:15:43 +0200

    detect content length or chunked

--- test/simple/conf/xauxi.lua
+++ test/simple/conf/xauxi.lua
@@ -3,7 +3,6 @@ Request = {
   method = "",
   uri = "",
   version = "",
-  headers = {}, 
   state = "header", 
   buf = "",
   getLine = function(self)
@@ -26,6 +25,29 @@ Request = {
   end
 }
 
+function headerTableNew()
+  local headerTable = {}
+  meta = {}
+  function meta.__newindex(t, k, v)
+    local entry = { key = k, val = v }
+    if type(k) == "string" then
+      k = string.lower(k)
+    end
+    rawset(t, k, entry)
+  end
+
+  function meta.__index(t, k)
+    if type(k) == "string" then
+      k = string.lower(k)
+    end
+    e = rawget(t, k)
+    return e
+  end
+
+  setmetatable(headerTable, meta)
+  return headerTable
+end
+
 connections = {}
 
 function http(connection, data, nextFilter)
@@ -35,6 +57,7 @@ function http(connection, data, nextFilter)
     else
       print("new connection")
       local r = Request
+      r.headers = headerTableNew()
       connections[connection] = r 
     end
     r = connections[connection]
@@ -43,21 +66,27 @@ function http(connection, data, nextFilter)
       r.buf = r.buf .. data
       line = r:getLine()
       while line do
-        if r.theRequest == nil then
-          r.theRequest = line
-          r.method, r.uri, r.version = string.match(line, "(%a+)%s([%w%p]+)%s%a+%p([%d%p]+)")
+        if string.len(line) > 0 then
+          if r.theRequest == nil then
+            r.theRequest = line
+            r.method, r.uri, r.version = string.match(line, "(%a+)%s([%w%p]+)%s%a+%p([%d%p]+)")
+          else
+            name, value = string.match(line, "([-.%a]+):%s([%w%p%s]+)")
+            -- TODO: overwrite table method of r.headers to be able to lookup case insensitiv
+            --       without storing the name lower case, want them untouched
+            r.headers[name] = value
+            print(r.headers[name].key, r.headers[name].val)
+          end
+          line = r:getLine()
         else
-          name, value = string.match(line, "([-.%a]+):%s([%w%p%s]+)")
-          -- TODO: overwrite table method of r.headers to be able to lookup case insensitiv
-          --       without storing the name lower case, want them untouched
-          print(name, value)
-        end
-        if string.len(line) == 0 then
           r.state = "body"
+          if r.headers["Content-Length"] then
+            print("Content-Length body")
+          elseif r.headers["Transfer-Encoding"].val:lower() == "chunked" then
+            print("Chunked Encoded body")
+          end
           nextFilter(r, r.buf)
           break
-        else
-          line = r:getLine()
         end
       end
     else
