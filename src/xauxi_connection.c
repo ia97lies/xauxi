ba54c7e6097a74a768e50c84c5253d311e467c11
Christian Liesch <liesch@gmx.ch>
2013-10-09 17:30:59 +0200

    connect implemented not tested

--- src/xauxi_connection.c
+++ src/xauxi_connection.c
@@ -228,7 +228,7 @@ struct luaL_Reg connection_methods[] = {
 /************************************************************************
  * Public
  ***********************************************************************/
-void xauxi_connection_connect(xauxi_object_t *object, const char *connect_to) {
+void xauxi_connection_connect(xauxi_object_t *object) {
   apr_pool_t *pool;
   apr_status_t status;
   xauxi_connection_t *connection;
@@ -236,7 +236,7 @@ void xauxi_connection_connect(xauxi_object_t *object, const char *connect_to) {
   char *scope_id;
   apr_port_t port;
   apr_sockaddr_t *sa;
-
+  const char *connect_to = object->name;
 
   xauxi_logger_t *logger = xauxi_get_logger(object->L);
   xauxi_global_t *global = xauxi_get_global(object->L);
@@ -250,40 +250,47 @@ void xauxi_connection_connect(xauxi_object_t *object, const char *connect_to) {
   connection->buffer = apr_brigade_create(pool, connection->alloc);
   if ((status = apr_parse_addr_port(&addr, &scope_id, &port, connect_to, pool))
       == APR_SUCCESS) {
-    /*
-status = apr_sockaddr_info_get(&sa, &addr, ly,
-                                          apr_port_t port,
-                                          apr_int32_t flags,
-                                          apr_pool_t *p);
-                                          */
-
-    if ((status = apr_socket_create(&connection->socket, APR_INET, SOCK_STREAM,
-            APR_PROTO_TCP, pool)) == APR_SUCCESS) {
-      if ((status = apr_socket_opt_set(connection->socket, APR_TCP_NODELAY, 
-              1)) == APR_SUCCESS) {
-        if ((status = apr_socket_timeout_set(connection->socket, 0)) 
-            == APR_SUCCESS) {
-          /* TODO: store client address in connection and log it */
-          xauxi_logger_log(logger, XAUXI_LOG_DEBUG, 0, "Accept connection");
-          connection->event = xauxi_event_socket(pool, connection->socket);
-          xauxi_event_get_pollfd(connection->event)->reqevents = APR_POLLIN | APR_POLLERR;
-          xauxi_event_register_read_handle(connection->event, _notify_read_data); 
-          xauxi_event_set_custom(connection->event, connection);
-          xauxi_dispatcher_add_event(global->dispatcher, connection->event);
+    if ((status = apr_sockaddr_info_get(&sa, addr, APR_UNSPEC, port, 
+            APR_IPV4_ADDR_OK, pool)) == APR_SUCCESS) {
+      if ((status = apr_socket_create(&connection->socket, APR_INET, SOCK_STREAM,
+              APR_PROTO_TCP, pool)) == APR_SUCCESS) {
+        if ((status = apr_socket_opt_set(connection->socket, APR_TCP_NODELAY, 
+                1)) == APR_SUCCESS) {
+          if ((status = apr_socket_timeout_set(connection->socket, 0)) 
+              == APR_SUCCESS) {
+            if ((status = apr_socket_connect(connection->socket, sa)) 
+                == APR_SUCCESS) {
+              /* TODO: store client address in connection and log it */
+              xauxi_logger_log(logger, XAUXI_LOG_DEBUG, 0, "Connection request pending");
+              connection->event = xauxi_event_socket(pool, connection->socket);
+              xauxi_event_get_pollfd(connection->event)->reqevents = APR_POLLIN | APR_POLLERR;
+              xauxi_event_register_read_handle(connection->event, _notify_read_data); 
+              xauxi_event_set_custom(connection->event, connection);
+              xauxi_dispatcher_add_event(global->dispatcher, connection->event);
+            }
+            else {
+              xauxi_logger_log(logger, XAUXI_LOG_ERR, status, 
+                  "Could not connect to remote host %s:%d", addr, port);
+            }
+          }
+          else {
+            xauxi_logger_log(logger, XAUXI_LOG_ERR, status, 
+                "Could not set socket nonblocking");
+          }
         }
         else {
           xauxi_logger_log(logger, XAUXI_LOG_ERR, status, 
-              "Could not set connection nonblocking");
+              "Could not set socket to nodelay");
         }
       }
       else {
         xauxi_logger_log(logger, XAUXI_LOG_ERR, status, 
-            "Could not set accepted connection to nodelay");
+            "Could not create connection");
       }
     }
     else {
       xauxi_logger_log(logger, XAUXI_LOG_ERR, status, 
-          "Could not create connection");
+          "Could not resolve hostname %s", addr);
     }
   }
   else {
