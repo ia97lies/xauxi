ccfd29515282132372cdbe52bc37dcf48cb44563
Christian Liesch <liesch@gmx.ch>
2013-11-04 22:07:59 +0100

    do not need c code so far, maybe later

--- distcheck.sh
+++
@@ -1,9 +0,0 @@
-TOP=`pwd`
-
-echo
-echo "  Make Distribution"
-CONFIG=""
-CFLAGS="-O0 -g -Wall -ansi -Wdeclaration-after-statement -Werror" ./configure $CONFIG
-make clean all
-make distcheck DISTCHECK_CONFIGURE_FLAGS="$CONFIG"
-
