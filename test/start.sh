12704340e5e9a753cf3a4c84b8181a6d5e671679
Christian Liesch <liesch@gmx.ch>
2013-11-15 23:27:32 +0100

    do log server start into start.log

--- test/start.sh
+++ test/start.sh
@@ -3,9 +3,11 @@ TOP=..
 
 . $TOP/config/config.sh
 
-$LUANODE $XAUXI_HOME/server/proxy/conf/xauxi.lua 2>&1 > $XAUXI_HOME/server/proxy/logs/start.log &
+> $XAUXI_HOME/server/proxy/logs/start.log
+$LUANODE $XAUXI_HOME/server/proxy/conf/xauxi.lua >> $XAUXI_HOME/server/proxy/logs/start.log 2>> $XAUXI_HOME/server/proxy/logs/start.log &
 echo $! > $XAUXI_HOME/server/proxy/run/.pid
 
-$LUANODE $XAUXI_HOME/server/content/conf/xauxi.lua 2>&1 > $XAUXI_HOME/server/content/logs/start.log &
+> $XAUXI_HOME/server/content/logs/start.log
+$LUANODE $XAUXI_HOME/server/content/conf/xauxi.lua >> $XAUXI_HOME/server/content/logs/start.log 2>> $XAUXI_HOME/server/content/logs/start.log &
 echo $! > $XAUXI_HOME/server/content/run/.pid
 
