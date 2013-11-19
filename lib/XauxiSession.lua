2563bbcff4a9b6f7522b5e471a6524719102ac2b
Christian Liesch <liesch@gmx.ch>
2013-11-19 17:22:36 +0100

    #33: Finished inmemory session store

--- lib/XauxiSession.lua
+++ lib/XauxiSession.lua
@@ -14,7 +14,8 @@
 -- limitations under the License.
 ------------------------------------------------------------------------------
 
-local xauxiSession = {}
+local sessionStore = {}
+local inmemoryDriver = { db = {} }
 local _driver
 local _timeout
 local _tinaltimeout
@@ -23,8 +24,12 @@ local _tinaltimeout
 -- Connect to a DB
 -- @param driver IN DB driver
 ------------------------------------------------------------------------------
-function xauxiSession.connect(driver, timout, finalTimeout)
-  _driver = driver
+function sessionStore.connect(driver, timout, finalTimeout)
+  if driver == nil then
+    _driver = inmemoryDriver
+  else
+    _driver = driver
+  end
   _timeout = timout
   _finalTimeout = finalTimeout
 end
@@ -33,11 +38,118 @@ end
 ------------------------------------------------------------------------------
 -- Set entry
 ------------------------------------------------------------------------------
-function xauxiSession.set(id, session)
-
-  _driver.set(key, value)
-  _driver.setTimeout(key, ttl)
+function sessionStore.set(id, session)
+  _driver.set(id, session)
 end
 
-return xauxiSession
+------------------------------------------------------------------------------
+-- Get entry
+------------------------------------------------------------------------------
+function sessionStore.get(id)
+  return _driver.get(id)
+end
+
+------------------------------------------------------------------------------
+-- Delete entry
+------------------------------------------------------------------------------
+function sessionStore.del(id)
+  return _driver.del(id)
+end
+
+------------------------------------------------------------------------------
+-- Delete entry
+------------------------------------------------------------------------------
+function sessionStore.noOfEntries()
+  return _driver.noOfEntries()
+end
+
+------------------------------------------------------------------------------
+-- Do delete all out dated entries
+------------------------------------------------------------------------------
+function sessionStore.maintenance()
+  return _driver.maintenance()
+end
+
+------------------------------------------------------------------------------
+-- Get builtin inmemory driver 
+------------------------------------------------------------------------------
+function sessionStore.getInmemoryDriver()
+  return inmemoryDriver
+end
+
+------------------------------------------------------------------------------
+-- Builtin inmemory driver
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+-- Set entry
+------------------------------------------------------------------------------
+function inmemoryDriver.set(key, value)
+  entry = { value, os.time(), os.time() }
+  inmemoryDriver.db[key] = entry
+end
+
+------------------------------------------------------------------------------
+-- Check if entry is valid
+------------------------------------------------------------------------------
+function inmemoryDriver.isValid(entry) 
+   if _timeout == 0 or os.time() - entry[2] < _timeout then
+    if _finalTimeout == 0 or os.time() - entry[3] < _finalTimeout then
+      return true
+    else
+      return false 
+    end
+  else
+    return false
+  end
+end
+
+------------------------------------------------------------------------------
+-- Get entry
+------------------------------------------------------------------------------
+function inmemoryDriver.get(key, value)
+  entry = inmemoryDriver.db[key]
+  if entry then
+    if inmemoryDriver.isValid(entry) then
+      entry[2] = os.time()
+      return entry[1]
+    else
+      inmemoryDriver.db[key] = nil
+      return nil
+    end
+  else
+    return nil
+  end
+end
+  
+------------------------------------------------------------------------------
+-- Delete entry
+------------------------------------------------------------------------------
+function inmemoryDriver.del(key)
+  inmemoryDriver.db[key] = nil
+end
+   
+------------------------------------------------------------------------------
+-- No of entries
+------------------------------------------------------------------------------
+function inmemoryDriver.noOfEntries()
+  count = 0
+  for _, v in pairs(inmemoryDriver.db) do
+    count = count + 1
+  end
+  return count
+end
+ 
+------------------------------------------------------------------------------
+-- Maintenance
+------------------------------------------------------------------------------
+function inmemoryDriver.maintenance()
+  count = 0
+  for id, entry in pairs(inmemoryDriver.db) do
+    if not inmemoryDriver.isValid(entry) then
+      inmemoryDriver.db[id] = nil
+    end
+  end
+end
+
+return sessionStore
 
--- test/test_session.lua
+++ test/test_session.lua
@@ -2,6 +2,80 @@ require "lunit"
 
 module(..., lunit.testcase, package.seeall)
 
-function test()
-  assert_true(false)
+local myTime = 0.0
+os.time = function()
+  return myTime
+end
+
+local session = require "XauxiSession"
+
+function test_set_get()
+  -- never expire
+  session.connect(session.getInmemoryDriver(), 0, 0)
+  session.set("key", "value")
+  assert_equal(session.get("key"), "value", "key not found") 
+end
+
+function test_set_get_100()
+  -- never expire
+  session.connect(session.getInmemoryDriver(), 0, 0)
+  for i = 1, 100 do
+    session.set("key"..i, "value"..i)
+  end
+  for i = 100, 1, -1 do
+    assert_equal(session.get("key"..i), "value"..i, "key not found") 
+  end
+end
+
+function test_set_get_timeout()
+  session.connect(session.getInmemoryDriver(), 10.0, 0)
+  session.set("key", "value")
+  assert_equal(session.get("key"), "value", "key not found")
+  myTime = myTime + 1.0
+  assert_equal(session.get("key"), "value", "key not found")
+  myTime = myTime + 9.9
+  assert_equal(session.get("key"), "value", "key not found")
+  myTime = myTime + 10.1 
+  assert_true(session.get("key") == nil)
+end
+
+function test_set_get_final_timeout()
+  session.connect(session.getInmemoryDriver(), 10.0, 20.0)
+  session.set("key", "value")
+  for i = 1,20 do
+    assert_equal(session.get("key"), "value", "key not found")
+    myTime = myTime + 1.0
+  end
+  myTime = myTime + 1.0 
+  assert_true(session.get("key") == nil)
+end
+
+function test_del()
+  session.connect(session.getInmemoryDriver(), 0, 0)
+  session.set("key", "value")
+  assert_equal(session.get("key"), "value", "key not found")
+  session.del("key")
+  assert_true(session.get("key") == nil)
+end
+
+function test_maintenance()
+  session.connect(session.getInmemoryDriver(), 10, 20)
+  for i = 1,100 do
+    session.set("key"..i, "value"..i)
+  end
+  assert_true(session.noOfEntries() == 100, string.format("found %d entries", session.noOfEntries()))
+  for i = 1,9 do
+    myTime = myTime + 1.0
+    session.maintenance()
+    assert_true(session.noOfEntries() == 100, string.format("found %d entries", session.noOfEntries()))
+  end
+  myTime = myTime + 1.0
+  session.maintenance()
+  assert_true(session.noOfEntries() == 0, string.format("found %d entries", session.noOfEntries()))
+end
+
+function test_convenience()
+  session.connect(nil, 0, 0)
+  session.set("key", "value")
+  assert_equal(session.get("key"), "value", "key not found") 
 end
