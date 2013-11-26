318bbecb8b780365504b5cf1f6e45520464ea982
Christian Liesch <liesch@gmx.ch>
2013-11-26 06:26:12 +0100

    new tasks

--- lib/xauxi/debugger.lua
+++ lib/xauxi/debugger.lua
@@ -8,9 +8,22 @@
 local info = require("xauxi.info")
 local http = require("luanode.http")
 local url = require("luanode.url")
+local querystring = require("luanode.querystring")
 local fs = require("luanode.fs")
 
 local _debugger = {}
+
+------------------------------------------------------------------------------
+-- Get variable value and scope
+------------------------------------------------------------------------------
+function _getVariable(variable)
+  local value
+  local found = false
+  local level = 2
+  for i = 1, math.huge do
+    
+  end
+end
 
 ------------------------------------------------------------------------------
 -- Listen to a port
@@ -18,13 +31,26 @@ local _debugger = {}
 function _debugger.listen(port)
   local server = http.createServer(function (server, req, res)
     local pathname = url.parse(req.url).pathname
+    local query = querystring.parse(req.url)
     if pathname == "/version" then
       res:writeHead(200, {["Content-Type"] = "text/json"})
-      res:write("{ version = "..info.version.." }")
+      res:write("{ \"message\" = \"Version "..info.version.."\" }")
+      res:finish()
+    elseif pathname == "/value" then
+      local variable = query["variable"]
+      if variable ~= nil then
+
+      else
+        res:writeHead(500, {["Content-Type"] = "text/json"})
+        res:write("{ \"message\" : \"Variable not specified\" }")
+        res:finish()
+      end
+      res:writeHead(200, {["Content-Type"] = "text/json"})
+      res:write("{ \"message\" = \"Version "..info.version.."\" }")
       res:finish()
     else
       res:writeHead(404, {["Content-Type"] = "text/json"})
-      res:write("{ notfound = 404 }")
+      res:write("{ \"message\" : \"Command not found\" }")
       res:finish()
     end
   end):listen(port)
