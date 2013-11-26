b129d4a55978f6f5ac355701321acc2058c1cb4c
Christian Liesch <liesch@gmx.ch>
2013-11-26 22:51:53 +0100

    Use xauxi also as a content provider

--- server/content/conf/xauxi.lua
+++ server/content/conf/xauxi.lua
@@ -1,29 +1,37 @@
 require "config"
 xauxi = require "xauxi.engine"
-http = require('luanode.http')
-log_file = require("logging.file")
-errorLogger = log_file("content_error.log")
 
-http.createServer(function(server, req, res)
-  server:addListener('error', function (self, msg, code)
-    errorLogger:error("%d %s(%d)", req.uniqueId, msg, code)
-  end)
-  if xauxi.location(req, "/test/luanode/hello") then
-    res:writeHead(200, {["Content-Type"] = "text/plain"})
-    res:finish("Hello World")
-  elseif xauxi.location(req, "/test/luanode/huge") then
-    res:writeHead(200, {["Content-Type"] = "text/plain"})
-    res:write("begin")
-    for i = 1,100 do
-      res:write("..............................................................................................................")
+xauxi.run {
+  serverRoot = XAUXI_HOME.."/server/content/logs",
+  errorLog = {
+    file = "error.log"
+  },
+
+  {
+    host = "localhost",
+    port = 9091,
+    transferLog = { 
+      file = "access.log", 
+      log = function(logger, req, res)
+        logger:info("%s %s %s %d User-Agent=\"%s\" Referer=\"%s\" T=%2f", req.uniqueId, req.method, req.url, req.statusCode, req.headers["user-agent"] or "<null>", req.headers["referer"] or "<null>", req.time.finish - req.time.start)
+      end 
+    },
+
+    map = function(conn, req, res)
+      if xauxi.location(req, "/test/luanode/hello") then
+        res:writeHead(200, {["Content-Type"] = "text/plain"})
+        res:finish("Hello World")
+      elseif xauxi.location(req, "/test/luanode/huge") then
+        res:writeHead(200, {["Content-Type"] = "text/plain"})
+        res:write("begin")
+        for i = 1,100 do
+          res:write("..............................................................................................................")
+        end
+        res:finish("end")
+      else
+        xauxi.sendNotFound(req, res)
+      end
     end
-    res:finish("end")
-  else
-    xauxi.sendNotFound(server, req, res)
-  end
-end):listen(9091)
-
-console.log('Server running at http://127.0.0.1:9091/')
-
-process:loop()
+  }
+}
 
