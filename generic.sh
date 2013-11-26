f300c6685e8cc681859a5c6098d77200d504bb75
Christian Liesch <liesch@gmx.ch>
2013-11-26 21:36:09 +0100

    #36 correct boost lib download, do it only if not there

--- generic.sh
+++ generic.sh
@@ -15,38 +15,45 @@ cd 3rdparty
 ###############################################################################
 boost_version_tar=`echo $BOOST_VERSION | awk -F. '{ printf "%s_%s_%s", $1, $2, $3 }'`
 boost_version_dir=$BOOST_VERSION
-if [ ! -f boost_${boost_version_tar}.tar.gz ]; then
-	wget http://sourceforge.net/projects/boost/files/boost/${boost_version_dir}/boost_${boost_version_tar}.tar.gz/download -O boost-${boost_version_dir}.tar.gz
-	ln -s boost-${boost_version_dir} boost
-	tar xzf boost-${boost_version_dir}.tar.gz
+if [ ! -d boost_${boost_version_tar} ]; then
+  if [ ! -f boost_${boost_version_tar}.tar.gz ]; then
+    wget http://sourceforge.net/projects/boost/files/boost/${boost_version_dir}/boost_${boost_version_tar}.tar.gz/download -O boost_${boost_version_tar}.tar.gz
+  fi
+  rm -f boost
+  tar xzf boost_${boost_version_tar}.tar.gz
+  ln -s boost_${boost_version_tar} boost
 fi
+
 cd boost
 ./bootstrap.sh
 ./b2
 cd ..
-rm -rf boost-*.tar.gz 
 
 ###############################################################################
 ## LuaNode
 ###############################################################################
-if [ ! -f LuaNode.zip ]; then
-	wget https://github.com/ignacio/LuaNode/archive/master.zip -O LuaNode.zip
-	ln -s LuaNode-master LuaNode
-	unzip -qq -o LuaNode.zip 
+if [ ! -d LuaNode-master ]; then
+  if [ ! -f LuaNode.zip ]; then
+    wget https://github.com/ignacio/LuaNode/archive/master.zip -O LuaNode.zip
+  fi
+  rm -f LuaNode
+  unzip -qq -o LuaNode.zip 
+  ln -s LuaNode-master LuaNode
 fi
 cd LuaNode/build
 cmake ../ -DBOOST_ROOT=$HOME/workspace/xauxi/3rdparty/boost
 make
 cd ../..
-rm -rf LuaNode.zip
 
 ###############################################################################
 ## lualogging
 ###############################################################################
-if [ ! -f lualogging-$LUALOGGING_VERSION ]; then
-	wget https://github.com/downloads/Neopallium/lualogging/lualogging-$LUALOGGING_VERSION.tbz 
-	ln -s lualogging-$LUALOGGING_VERSION lualogging
-	tar xjf lualogging-$LUALOGGING_VERSION.tbz
+if [ ! -d lualogging-$LUALOGGING_VERSION ]; then
+  if [ ! -f lualogging-$LUALOGGING_VERSION.tbz ]; then
+    wget https://github.com/downloads/Neopallium/lualogging/lualogging-$LUALOGGING_VERSION.tbz 
+  fi
+  rm -f lualogging
+  tar xjf lualogging-$LUALOGGING_VERSION.tbz
+  ln -s lualogging-$LUALOGGING_VERSION lualogging
 fi
-rm -rf lualogging-$LUALOGGING_VERSION.tbz
 
