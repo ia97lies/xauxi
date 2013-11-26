1a0d8d0d5d5549cc3a35686d8b80bcd264970232
Christian Liesch <ia97lies@users.sourceforge.net>
2013-11-26 16:45:41 +0100

    Add logs and run directory for proxy and content server

--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ printf "     Lua path"
 echo "package.path = package.path..\";${XAUXI_HOME}/lib/?.lua;${LUALOGGER_HOME}/src/?.lua;./?.lua\"" > test/config.lua
 echo "XAUXI_HOME = \"${XAUXI_HOME}\"" >> test/config.lua
 printf " ok\n"
+mkdir -p server/proxy/logs
+mkdir -p server/proxy/run
+mkdir -p server/content/logs
+mkdir -p server/content/run
